5b9b35523cdd260f192036c27191f79917d8f42e
Glenn Halls <gjhalls@gmail.com>
2022-12-01 10:07:52 +1100

    Add Layout

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,24 +1,25 @@
 package com.example.lemonade
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,14 +35,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ShowImage() {
+
+    val borderColor = Color(105, 205, 216)
+
+
+    var state by remember { mutableStateOf(1) }
+
     Column (
         modifier = Modifier
             .fillMaxSize()
             .wrapContentSize(Alignment.Center),
     ) {
+        Text(
+            text = "hello",
+            style = TextStyle(fontSize = 18.sp, textAlign = TextAlign.Center),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         Image(
             painter = painterResource(R.drawable.lemon_tree),
-            contentDescription = "TBD")
+            contentDescription = "TBD",
+            modifier = Modifier
+                .clickable(onClick = {
+                    state = (1..4).random()
+                })
+                .border(2.dp, borderColor, RoundedCornerShape(4.dp))
+        )
 
     }
 }
